05170897fc8aec43fb18c9f5a154c2a34ef1348b
David Strayhorn <david.strayhorn4@gmail.com>
2024-04-12 00:45:46 -0400

    import a few packages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Brainstorm',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
